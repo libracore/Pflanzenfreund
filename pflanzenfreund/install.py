5a72123fb51b6717d6633d889937fcdb424548f5
Joelios <joel.mesmer@libracore.com>
2018-06-27 11:56:25 +0200

    bug fixes in install.py, also added progressbar

--- pflanzenfreund/install.py
+++ pflanzenfreund/install.py
@@ -1,22 +1,21 @@
 from __future__ import print_function, unicode_literals
 
 import frappe
+from frappe.utils import update_progress_bar
 
 def after_install():
-	print('Deactivate Role "Website Manager"...')
 	disable_website_manager()
-	print('Define Permissions for "Pflanzenfreund Manager"...')
 	set_permissions_for_pflanzenfreund_manager()
-	print('Change Module definition from "Website" to "Pflanzenfreund"...')
-	change_module_def()
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 	print('Installation of "Pflanzenfreund" successfull')
+	print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 	
 def disable_website_manager():
+	update_progress_bar('Deactivate Role "Website Manager"', 0, 1)
 	sql_query = """UPDATE `tabRole`
 		SET `disabled` = '1'
 		WHERE `role_name` = 'Website Manager'"""
 	frappe.db.sql(sql_query)
-	print("...deactivated...")
 	
 def set_permissions_for_pflanzenfreund_manager():
 	docs_for_permission = [
@@ -27,13 +26,8 @@ def set_permissions_for_pflanzenfreund_manager():
 		'Portal Settings',
 		'Help Category', 'Help Article'
 	]
+	i = 0
 	for doc in docs_for_permission:
 		frappe.permissions.add_permission(doc, "Pflanzenfreund Manager")
-	print("...Permissions defined...")
-	
-def change_module_def():
-	sql_query = """UPDATE `tabDocType`
-		SET `module` = 'Pflanzenfreund'
-		WHERE `module` = 'Website'"""
-	frappe.db.sql(sql_query)
-	print("...Module changed...")
+		update_progress_bar('Define Permissions for "Pflanzenfreund Manager"', i, len(docs_for_permission))
+		i = i + 1
