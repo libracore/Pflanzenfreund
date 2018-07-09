fc19643b184c1522a776f48b4cbe4234428c4b24
lasalesi <larsericmueller@gmail.com>
2018-07-09 12:05:22 +0200

    add sync log and sync queue

--- pflanzenfreund/pflanzenfreund/doctype/greeninfo/greeninfo.js
+++ pflanzenfreund/pflanzenfreund/doctype/greeninfo/greeninfo.js
@@ -3,6 +3,21 @@
 
 frappe.ui.form.on('GreenInfo', {
 	refresh: function(frm) {
-
+        frm.add_custom_button(__("Sync"), function() 
+		{
+			sync(frm);
+		}).addClass("btn-success");
 	}
 });
+
+function sync(frm) {
+	frappe.call({
+		method: 'sync',
+		doc: frm.doc,
+		callback: function(response) {
+			if (response.message) {
+				frappe.show_alert(response.message);
+			}
+		}
+	});
+}
--- pflanzenfreund/pflanzenfreund/doctype/greeninfo/greeninfo.py
+++ pflanzenfreund/pflanzenfreund/doctype/greeninfo/greeninfo.py
@@ -5,6 +5,39 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _
+from frappe.utils.background_jobs import enqueue
 
 class GreenInfo(Document):
-	pass
+    def sync(self):
+        # enque sync
+        add_log(_("Sync started"), _("Sync process started"))
+        kwargs={
+            'config': self
+        }
+        enqueue("pflanzenfreund.pflanzenfreund.doctype.greeninfo.greeninfo.sync",
+            queue='long',
+            timeout=15000,
+            **kwargs)
+        return _("Sync started. See log for details.")
+
+# sync data with GreenInfo
+def sync_greeninfo(config):
+    # get configuration
+    input_file = config.input_file
+    output_file = config.output_file
+    return
+
+def import_data(filename):
+    pass
+	
+def export_data(filename):
+    pass
+
+# create a new log entry
+def add_log(title, message):
+    new_log = frappe.get_doc({'doctype': 'GreenInfo Sync Log'})
+    new_log.title = title
+    new_log.message = message
+    new_log.insert()
+    return
