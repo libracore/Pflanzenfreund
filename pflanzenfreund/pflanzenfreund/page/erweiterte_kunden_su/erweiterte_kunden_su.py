a29782acdf7e1afc98b80d4c682b9e831d8c68a5
Joelios <joel.mesmer@libracore.com>
2018-07-25 11:36:07 +0200

    new function search-page

---
+++ pflanzenfreund/pflanzenfreund/page/erweiterte_kunden_su/erweiterte_kunden_su.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-2018, libracore and contributors
+# License: AGPL v3. See LICENCE
+
+from __future__ import unicode_literals
+import frappe
+from frappe import throw, _
+
+@frappe.whitelist()
+def get_all_infos():
+	customer_query = """SELECT cus.customer_name, cus.customer_name,  adr.address_line1, adr.pincode, adr.city
+		FROM ((`tabAddress` AS adr
+		INNER JOIN `tabDynamic Link` AS dyn ON adr.name = dyn.parent)
+		INNER JOIN `tabCustomer` AS cus ON dyn.link_name = cus.name)"""
+	return frappe.db.sql(customer_query, as_dict=1)
