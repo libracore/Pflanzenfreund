7172d257958b7199579353d191467c79fd12512f
lasalesi <larsericmueller@gmail.com>
2018-08-20 23:45:19 +0200

    external auth function

--- pflanzenfreund/pflanzenfreund/extauth.py
+++ pflanzenfreund/pflanzenfreund/extauth.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018, libracore and contributors
 # For license information, please see license.txt
+#
+# Call API as
+#   /api/method/pflanzenfreund.pflanzenfreund.extauth.check_credentials
+#   args { user, password }
 
+import frappe
 from frappe.utils.password import check_password
 
 # API function for external authentication
@@ -9,9 +14,10 @@ frappe.whitelist(allow_guest=True)
 def check_credentials(user, password):
 	if not user or not password:
 		frappe.throw("Please provide credentials")
-		return
+		return { 'status': 'missing credentials' }
 	
 	user = check_password(user, password)
 	if user:
-		role = frappe.get_value("User", user, "pflanzenfreund_role")
-		return {'user': user, 'role': role}
+		role = frappe.get_value("User", user, "pflanzenfreund_group")
+		return {'status': 'OK', 'user': user, 'role': role}
+	return { 'status': 'failed' }
