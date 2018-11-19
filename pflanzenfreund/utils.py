2d2f10d1a441d6eedbf17ef2440fdab87fc44392
Joelios <joel.mesmer@libracore.com>
2018-11-19 10:45:08 +0100

    board task 1364

--- pflanzenfreund/utils.py
+++ pflanzenfreund/utils.py
@@ -652,6 +652,7 @@ def _createNewInvoices_abo_rechnungslauf(start, end, abo_type, bullet_type, bull
 		#create new abo
 		new_abo = frappe.new_doc("Pflanzenfreund Abo")
 		new_abo.update({
+			"abo_ist_verlaengerung": 1,
 			"customer": old_abo.customer,
 			"customer_address": old_abo.customer_address,
 			"customer_letter_salutation": old_abo.customer_letter_salutation,
